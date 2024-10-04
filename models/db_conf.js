aa935ba579b18159a66cb15b0bad5b58f37cd4b8
coita <larisa.coita@student.vinci.be>
2024-10-04 11:48:32 +0200

    larisa

--- models/db_conf.js
+++ models/db_conf.js
@@ -1,6 +1,6 @@
 // file db_conf.js in models
 
-const db = require('better-sqlite3')('C:/vinci/Devops/backup-web1-23_24/db/exoplanets.db', { verbose: console.log });
+const db = require('better-sqlite3')('/Users/larisacoita/devops-partage/demo-devops/db/exoplanets.db', { verbose: console.log });
 // const db = require('better-sqlite3')('/home/olivier/exoplanets.db', { verbose: console.log });
 
 module.exports = db;
